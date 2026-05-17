f97fa860da04b81dbcc755506bd5f707dcce34c3
terpiriyapong2002-code <terpiriyapong2002@gmail.com>
2026-05-17 13:10:30 +0700

    Update build config for new repository URL

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/THE-IDOL-SIMULATOR/',
+  base: '/MYYYYYIDOL/',
   plugins: [
     react(),
     VitePWA({
